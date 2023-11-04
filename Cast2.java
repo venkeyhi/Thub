687300e882a8faa309d80079f0ea1df93f609471
venkeyhi <146825469+venkeyhi@users.noreply.github.com>
2023-11-04 14:23:37 +0530

    Add files via upload

---
+++ Cast2.java
@@ -0,0 +1,12 @@
+class Cast2
+{
+ public static void main(String args[])
+  {
+    int x=3,n=5,sum=1;
+
+    for(int i=1;i<=n;i++)
+       sum=sum+(int)Math.pow(x,i);
+
+ System.out.println(sum);
+}
+}
